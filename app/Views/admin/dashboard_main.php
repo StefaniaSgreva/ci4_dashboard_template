e02a8457f0278628a69b464fba5ef00db0ad5a88
Stefania Sgreva <stefaniasgreva@gmail.com>
2023-06-06 15:28:33 +0200

    Media Queries

--- app/Views/admin/dashboard_main.php
+++ app/Views/admin/dashboard_main.php
@@ -1,10 +1,20 @@
 <?= $this->extend('layouts/main.php') ?>
 <?= $this->section('content') ?>
 
-<div class="dashboard">
-    <?= $this->include('admin/sidebar') ?>
-    <?= $this->include('admin/main_content') ?>
+<div class="dashboard_wrapper">
+    <!-- Mobile Button  -->
+    <input type="checkbox" class="mobile_btn" id="mobile_btn">
+    <label for="mobile_btn" class="mobile_label">
+        <div class="label_icons flex">
+            <i class="uil uil-bars"></i>
+            <i class="uil uil-times"></i>
+        </div>
+    </label>
 
+    <div class="dashboard">
+        <?= $this->include('admin/sidebar') ?>
+        <?= $this->include('admin/main_content') ?>
+    </div>
 </div>
 
 <?= $this->endSection() ?>
